3ec82f7f6ef225822e42af68cf593f841b71f124
Zaher Madi <zahermadi@yahoo.fr>
2023-12-06 21:00:30 +0100

    presentation initié

--- juice/src/app/app.component.ts
+++ juice/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PresentationComponent } from './presentation/presentation.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,SignupComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,SignupComponent, HttpClientModule,PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
