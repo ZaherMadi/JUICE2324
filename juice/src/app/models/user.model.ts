6dff049aa9900d0aa2cf44210f65bfd265064acf
Zaher Madi <zahermadi@yahoo.fr>
2023-12-20 11:47:29 +0100

    HTTP Services added

---
+++ juice/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+
+export class User {
+    constructor(
+        public gender: string,
+        public name: string,
+        public firstname: string,
+        public email: string,
+        public phone: number,
+    ){}
+}
--- juice/src/app/signup/signup.component.ts
+++ juice/src/app/signup/signup.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { User } from '../models/user.model';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  @Input() myUser : User = new User('', '', '', '', 0);
+
+  onModelChange() {
+    console.log('Current User data:', this.myUser);
+  }
+  
+
+  PostAPI() {
+    console.log("PostAPI")
+  }
+
 
 }
