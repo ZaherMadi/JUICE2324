99eff504ea7de9bab8825962981b6503eb809c33
Zaher Madi <zahermadi@yahoo.fr>
2023-12-20 18:24:33 +0100

    http Service down , method post/get directly in signupcomponents

--- juice/src/app/app.component.ts
+++ juice/src/app/app.component.ts
@@ -1,9 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
-import { HttpClientModule } from '@angular/common/http';
 import { PresentationComponent } from './presentation/presentation.component';
 import { TournoipresentationComponent } from './tournoipresentation/tournoipresentation.component';
 
@@ -11,7 +11,7 @@ import { TournoipresentationComponent } from './tournoipresentation/tournoiprese
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,SignupComponent, HttpClientModule,PresentationComponent,TournoipresentationComponent],
+  imports: [CommonModule, RouterOutlet,HttpClientModule, HeaderComponent,SignupComponent,PresentationComponent,TournoipresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- juice/src/app/signup/signup.component.ts
+++ juice/src/app/signup/signup.component.ts
@@ -1,13 +1,15 @@
+// import { HttpService } from '../services/http.service';
+
 import { Component, Input } from '@angular/core';
 import { User } from '../models/user.model';
 import { FormsModule } from '@angular/forms';
-import { HttpService } from '../services/http.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; // Import HttpClientModule
 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule , HttpClientModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
@@ -18,18 +20,58 @@ export class SignupComponent {
     console.log('Current User data:', this.myUser);
   }
   
-  constructor(private httpService: HttpService) {}
+  constructor(private httpClient: HttpClient) {}
 
   PostAPI() {
-    this.httpService.postUser(this.myUser).subscribe({
+    this.httpClient.post('https://jsonplaceholder.typicode.com/users', this.myUser).subscribe({
       next: (response) => {
         console.log('User created:', response);
       },
       error: (error) => {
         console.error('Error creating user:', error);
       }
-    });
+    })
+    
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe({
+      next: (response) => {
+      console.log('Donnes recu', response);
+    },
+    error: (error) => {
+      console.error('Error get', error);
+    }
+  })
+    ;
+
+
+    
   }
 
 
-}
+
+
+
+
+
+
+
+
+
+
+  // constructor(private httpService: HttpService) {}
+
+  // PostAPI() {
+  //   this.httpService.postUser(this.myUser).subscribe({
+  //     next: (response) => {
+  //       console.log('User created:', response);
+  //     },
+  //     error: (error) => {
+  //       console.error('Error creating user:', error);
+  //     }
+  //   });
+    
+
+    //peutetre ne pas faire le service et utilisé individuellement pck ca marche p 
+  }
+
+
+// }
